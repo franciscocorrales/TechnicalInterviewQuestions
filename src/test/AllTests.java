096ba3181ddc9f988cc667f5ba3384e13f2df00c
Francisco Corrales Morales <fcm200837354@gmail.com>
2016-08-03 16:02:26 -0600

    Begin process of jUnit.

---
+++ src/test/AllTests.java
@@ -0,0 +1,11 @@
+package src.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class AllTests {
+
+}
